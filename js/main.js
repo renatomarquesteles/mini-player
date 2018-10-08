282b0390625bdb939efba4272f709cbfea693b9b
unknown <renatomarquesteles@gmail.com>
2018-10-08 07:47:34 -0300

    cabecalho e menu

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function sbOpen() {
+    document.getElementById("sb-musicas").style.display = "block";
+}
+function sbClose() {
+    document.getElementById("sb-musicas").style.display = "none";
+}
