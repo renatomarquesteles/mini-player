bbe980bc218723763b7b452dffff6ba222ff1db2
Renato Marques Teles <renato.teles@smn.domain>
2018-10-08 15:50:59 -0300

    visual botoes, sidebars, listas etc

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
-function sbOpen() {
-    document.getElementById("sb-musicas").style.display = "block";
+var musicas = [
+    "Arctic Monkeys - Do I Wanna Know.mp3",
+    "Drake - God's Plan.mp3",
+    "Drake - Legend.mp3",
+    "Drake - Nice For What.mp3",
+    "Kendrick Lamar - HUMBLE.mp3",
+    "Migos - Walk It Talk It ft. Drake.mp3"    
+]
+var capas = [
+    "godsplan.jpg",
+    "legend.jpg",
+    "niceforwhat.jpg",
+    "walkittalkit.jpg",
+    "doiwannaknwo.jpg",
+    "humble.jpg"
+]
+var audio = new Audio('audioFile');
+audio.play();
+
+
+function openSlideMenuL() {
+    document.getElementById("menu-musicas").style.width = '200px';
+    document.getElementById("content").style.marginLeft = '200px';
 }
-function sbClose() {
-    document.getElementById("sb-musicas").style.display = "none";
+function closeSlideMenuL() {
+    document.getElementById("menu-musicas").style.width = '0';
+    document.getElementById("content").style.marginLeft = '0';
+}
+function openSlideMenuR() {
+    document.getElementById("menu-playlists").style.width = '200px';
+    document.getElementById("content").style.marginLeft = '-200px';
+}
+function closeSlideMenuR() {
+    document.getElementById("menu-playlists").style.width = '0';
+    document.getElementById("content").style.marginLeft = '0';
 }
