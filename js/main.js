e45d5891b1c748994850b59fa41c6260625f8862
Renato Marques Teles <renato.teles@smn.domain>
2018-10-09 10:43:22 -0300

    barras de reproducao e volume adicionadas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
+var audio;
 var musicas = [
-    "Arctic Monkeys - Do I Wanna Know.mp3",
-    "Drake - God's Plan.mp3",
-    "Drake - Legend.mp3",
-    "Drake - Nice For What.mp3",
-    "Kendrick Lamar - HUMBLE.mp3",
-    "Migos - Walk It Talk It ft. Drake.mp3"    
+    "music/Arctic Monkeys - Do I Wanna Know.mp3",
+    "music/Drake - God's Plan.mp3",
+    "music/Drake - Legend.mp3",
+    "music/Drake - Nice For What.mp3",
+    "music/Kendrick Lamar - HUMBLE.mp3",
+    "music/Migos - Walk It Talk It ft. Drake.mp3"
 ]
 var capas = [
     "godsplan.jpg",
@@ -14,23 +15,47 @@ var capas = [
     "doiwannaknwo.jpg",
     "humble.jpg"
 ]
-var audio = new Audio('audioFile');
-audio.play();
-
-
-function openSlideMenuL() {
+window.onload = function () {
+    document.getElementById("playBtn").addEventListener("click", playMusic);
+    document.getElementById("pauseBtn").addEventListener("click", pauseMusic);
+    audio = new Audio(musicas[0]);
+}
+function playMusic() {
+    audio.play();
+    document.getElementById("playBtn").style.display = "none";
+    document.getElementById("pauseBtn").style.display = "initial";
+}
+function pauseMusic() {
+    audio.pause();
+    document.getElementById("playBtn").style.display = "initial";
+    document.getElementById("pauseBtn").style.display = "none";
+}
+// function fillBar() {
+//     var elem = document.getElementById("progBar"); 
+//     var width = 1;
+//     var id = setInterval(frame, 100);
+//     function frame() {
+//         if (width >= 100) {
+//             clearInterval(id);
+//         } else {
+//             width++; 
+//             elem.style.width = width + '%'; 
+//         }
+//     }
+// }
+function openSlideMenuL() {     // Função para abrir a sidebar no lado esquerdo (left)
     document.getElementById("menu-musicas").style.width = '200px';
     document.getElementById("content").style.marginLeft = '200px';
 }
-function closeSlideMenuL() {
+function closeSlideMenuL() {    // Função para fechar a sidebar no lado esquerdo (left)
     document.getElementById("menu-musicas").style.width = '0';
     document.getElementById("content").style.marginLeft = '0';
 }
-function openSlideMenuR() {
+function openSlideMenuR() {     // Função para abrir a sidebar no lado direito (right)
     document.getElementById("menu-playlists").style.width = '200px';
     document.getElementById("content").style.marginLeft = '-200px';
 }
-function closeSlideMenuR() {
+function closeSlideMenuR() {    // Função para fechar a sidebar no lado direito (right)
     document.getElementById("menu-playlists").style.width = '0';
     document.getElementById("content").style.marginLeft = '0';
 }
