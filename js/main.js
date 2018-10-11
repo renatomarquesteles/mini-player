2a8e693ec47d473f2ea74586f5d014a410329ffc
Renato Marques Teles <renato.teles@smn.domain>
2018-10-11 16:01:32 -0300

    melhorias lista de reproducao e capa

--- js/main.js
+++ js/main.js
@@ -1,28 +1,22 @@
-var audio,              // Variável que irá receber as músicas
+var audio = null,              // Variável que irá receber as músicas
     music_index = 0,    // Índice de cada música no array de músicas
     shuffle = false,    // Variável que indica se o botão de shuffle está ativado ou não
+    loop = false,       // Variável que indica se o botão de loop está ativado ou não
     showVol = false,    // Variável que indica se a barra de volume está visível ou não
     local_M = "music/", // Variável que indica o local da música
     ext_M = ".mp3",     // Variável que indica a extensão da música
     musicList = [       // Array de músicas
-        "Arctic Monkeys - Do I Wanna Know",
-        "Drake - Nice For What",
-        "Drake - Legend",
-        "Migos - Walk It Talk It ft. Drake",
-        "Kendrick Lamar - HUMBLE",
-        "Drake - God's Plan"
+        {nome:"Arctic Monkeys - Do I Wanna Know", artista:"Arctic Monkeys", ano:"2013", genero:"Indie Rock", capa:"WannaKnow"},
+        {nome:"Drake - Nice For What", artista:"Drake", ano:"2018", genero:"Hip-Hop", capa:"Nice"},
+        {nome:"Drake - Legend", artista:"Drake", ano:"2015", genero:"Hip-Hop", capa:"Legend"},
+        {nome:"Migos - Walk It Talk It ft. Drake", artista:"Migos", ano:"2018", genero:"Trap, Hip-Hop", capa:"WalkIt"},
+        {nome:"Kendrick Lamar - HUMBLE", artista:"Kendrick Lamar", ano:"2017", genero:"Hip-Hop", capa:"Humble"},
+        {nome:"Drake - God's Plan", artista:"Drake", ano:"2018", genero:"Pop, Trap", capa:"GodsPlan"}
     ],
     local_C = "img/",   // Variável que indica o local da imagem de capa
     ext_C = ".jpg",     // Variável que indica a extensão da imagem de capa
-    capas = [           // Array com as imagens de capa de cada música
-        "godsplan",
-        "legend",
-        "niceforwhat",
-        "walkittalkit",
-        "doiwannaknwo",
-        "humble"
-    ],
     repList = [];   // Lista de reprodução
+    playlists = [];
 
 function showModal(x) {
     document.querySelector(("#m"+x+"Modal")).style.display = "block";
@@ -62,36 +56,56 @@ window.onload = function () {
     document.getElementById("sliderVolume").addEventListener("change", volume);
     document.getElementById("sliderMusica").addEventListener("change", posMusic);
     document.getElementById("btnVolume").addEventListener("click", btnVol);
-    audio = new Audio(local_M + musicList[music_index] + ext_M);
+    audio = new Audio(local_M + musicList.nome[music_index] + ext_M);
 }
 function addRep(index) {
     repList.push(musicList[index]);
     let aux = "";
     for (let i = 0; i < repList.length; i++) {
-        aux += "<p>" + repList[i] + "</p>";
+        aux += "<div class='repLista'><p id='list"+i+"' onclick='selectMusic("+i+")'>" + repList[i].nome + "</p><i class='fas fa-times' onclick='removeMusic("+i+")'></i></div>";
     }
     document.getElementById("repList").innerHTML = aux;
     closeModal(index);
-    closeSlideMenuL();
+}
+function removeMusic(x) {
+    // repList.splice(x,1);
+}
+function selectMusic(x) {
+    music_index = x;
+    stopMusic();
+    setMusic();
+    playMusic();
 }
 function clearRep() {
     repList = [];
 }
 function setMusic() {
-    audio = new Audio(local_M + repList[music_index] + ext_M);
+    audio = new Audio(local_M + repList[music_index].nome + ext_M);
+}
+function colorRep() {
+    for(let i = 0; i<repList.length; i++){
+        document.getElementById(("list"+i)).style.color = "#E0E0E0";
+    }
+    document.getElementById(("list"+music_index)).style.color = "#00B0FF";
 }
 function playMusic() {
-    if (repList.length == 0) {
-        alert("Você deve adicionar uma música à lista de reprodução");
-        openSlideMenuL();
-    }
-    else {
+    if(audio == null){
         setMusic();
+    }
+    
         audio.play();
         document.getElementById("playBtn").style.display = "none";
         document.getElementById("pauseBtn").style.display = "initial";
+        document.getElementById("info-musica").innerHTML = repList[music_index].nome;
+        document.getElementById("info-artista").innerHTML = repList[music_index].artista;
+        document.getElementById("info-ano").innerHTML = repList[music_index].ano;
+        document.getElementById("info-genero").innerHTML = repList[music_index].genero;
+        document.getElementById("capa").innerHTML = '<img src="' + local_C + repList[music_index].capa + ext_C + '"/>';
+        colorRep();
         setInterval(timer, 1000);
-    }
+    
+    closeSlideMenuR();
+    closeSlideMenuL();
 }
 function pauseMusic() {
     audio.pause();
@@ -106,30 +120,47 @@ function stopMusic() {
     document.getElementById("sliderMusica").value = 0;
 }
 function nextMusic() {
-    if (music_index == repList.length - 1)
-        music_index = 0;
-    else {
-        music_index++;
+    if(loop == false){
+        if(shuffle == false){
+            if (music_index == repList.length - 1){
+                music_index = 0;
+            }
+            else {
+                music_index++;
+            }
+        }
+        else {
+            music_index = Math.floor(Math.random() * repList.length);
+        }
     }
     stopMusic();
+    setMusic();
     playMusic();
 }
 function prevMusic() {
-    if (music_index == 0)
-        music_index = repList.length - 1;
-    else {
-        music_index--;
+    if(loop == false){
+        if(shuffle == false){
+            if (music_index == 0)
+                music_index = repList.length - 1;
+            else {
+                music_index--;
+            }
+        }
+        else {
+            music_index = Math.floor(Math.random() * repList.length);
+        }
     }
     stopMusic();
+    setMusic();
     playMusic();
 }
 function loopMusic() {
-    if (audio.loop == false) {
-        audio.loop = true;
+    if (loop == false) {
+        loop = true;
         document.getElementById("repeatBtn").style.color = "#00B0FF";
     }
     else {
-        audio.loop = false;
+        loop = false;
         document.getElementById("repeatBtn").style.color = "#E0E0E0";
     }
 }
@@ -164,6 +195,28 @@ function btnVol() {
         document.querySelector('.barraVol').style.display = "none";
     }
 }
+function playNow(x) {
+    if(repList.length != 0){
+        stopMusic();
+        clearRep();
+    }
+    addRep(x);
+    setMusic();
+    playMusic();
+}
+function saveList() {
+    playlists.push(repList);
+    let aux = "";
+    for (let i = 0; i < playlists.length; i++) {
+        aux += "<p id='playlist"+i+"' onclick='showMusics()'> Playlist "+i+"</p>";
+        for (let j = 0; j < repList.length; j++){
+            aux += "<p id='pl"+i+"music"+j+"' style='display: none'>"+ repList[j].nome +"</p>";
+        }
+    }
+    document.getElementById("playlists").innerHTML = aux;
+}
+function showMusics() {
+}
 function timer() {
     atual = (audio.currentTime).toFixed(0);
     final = (audio.duration).toFixed(0);
@@ -171,12 +224,20 @@ function timer() {
     document.getElementById("timerF").innerHTML = converte(final);
     document.getElementById("sliderMusica").value = (atual / final) * 100;
     if (atual == final) {
-        if (shuffle == false) {
-            nextMusic();
+        if(loop == false){
+            if (shuffle == false) {
+                nextMusic();
+            }
+            else {
+                music_index = Math.floor(Math.random() * repList.length);
+                stopMusic();
+                setMusic();
+                playMusic();
+            }
         }
         else {
-            music_index = Math.floor(Math.random() * repList.length);
             stopMusic();
+            setMusic();
             playMusic();
         }
     }
